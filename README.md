79dde889223b49ac232fa886739546ecd997f3b5
pedroaugust0 <rosso.pedroaugusto@gmail.com>
2016-09-30 22:57:31 -0300

    Changes on .gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-# PIC18F4520asm
-Programas Exemplos para PIC18F4520 desenvolvido em Assembly no MPLab X e simulado no Proteus 8.5.
--- delayWithCounter/build/default/production/main_asm.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/main_asm.o:  \
-	/home/pedro/MEOCloud/UFSC/2016.1/SOE/Lista-de-Exercicios-P1/8/main_asm.asm  \
-	/opt/microchip/mplabx/v3.30/mpasmx/p18f4520.inc  \
-
